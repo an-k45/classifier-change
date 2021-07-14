56a0e2e11e980f10d22969403a6e70f8829c5c39
an-k45 <an_k27@protonmail.com>
2021-07-14 13:52:56 -0400

    Add command line parsing

--- main.py
+++ main.py
@@ -6,6 +6,21 @@ import os
 import get_csvs as getter
 import homophony_counter as counter
 
+def get_syntax(syntax_type):
+    """ Return the syntax specification 
+    """
+    syntax = ["cl n", "cl adv n", "cl adj n"]
+    if syntax_type == "num":
+        syntax = ["num " + s for s in syntax]
+    elif syntax_type == "dem":
+        syntax = ["dem " + s for s in syntax]
+    elif syntax_type == "det":
+        syntax = ["det " + s for s in syntax]
+    elif syntax_type == "not_noun_all":
+        # POS_tags = ['pro:per', 'v:aux', 'conj', 'co', 'prep', 'n:relat', 'v:cop', 'pro:dem', 'v:resc', 'L2', 'pro:wh', 'v:dirc', 'n:fam', 'post', 'chi', 'v', 'adv', 'n:prop', 'poss', 'asp', 'det', 'adv:wh', 'adj', 'cl', 'num', 'n', 'on', 'cleft', 'sfp', 'nom', 'co:int']
+        syntax = ["cl (?!(?:adj|adv))[^n][a-z:]*", "cl adv [^n][a-z:]*", "cl adj [^n][a-z:]*"]
+    return syntax
+
 def handle_output_path(collection, language, want_children, syntax_type):
     """ Return output path, and create directories if they don't exist. 
     """
@@ -23,22 +38,19 @@ def handle_output_path(collection, language, want_children, syntax_type):
     return "./output/{}/{}/{}_{}".format(corpora_type, syntax_type, collection, language)
 
 if __name__ == "__main__":
-    # TODO: Parse command line arguments
-    collection = "Chinese"
-    language = "Mandarin"
-    want_children = False
-    syntax_type = "not_noun_all"
+    parser = argparse.ArgumentParser()
+    parser.add_argument(dest='collection', nargs='?', default='Chinese')
+    parser.add_argument(dest='language', nargs='?', default='Mandarin')
+    parser.add_argument(dest='want_children', nargs='?', default='no')
+    parser.add_argument(dest='syntax_type', nargs='?', default='noun_all')
+    args = parser.parse_args()
 
-    syntax = ["cl n", "cl adv n", "cl adj n"]
-    if syntax_type == "num":
-        syntax = ["num " + s for s in syntax]
-    elif syntax_type == "dem":
-        syntax = ["dem " + s for s in syntax]
-    elif syntax_type == "det":
-        syntax = ["det " + s for s in syntax]
-    elif syntax_type == "not_noun_all":
-        # POS_tags = ['pro:per', 'v:aux', 'conj', 'co', 'prep', 'n:relat', 'v:cop', 'pro:dem', 'v:resc', 'L2', 'pro:wh', 'v:dirc', 'n:fam', 'post', 'chi', 'v', 'adv', 'n:prop', 'poss', 'asp', 'det', 'adv:wh', 'adj', 'cl', 'num', 'n', 'on', 'cleft', 'sfp', 'nom', 'co:int']
-        syntax = ["cl (?!(?:adj|adv))[^n][a-z:]*", "cl adv [^n][a-z:]*", "cl adj [^n][a-z:]*"]
+    collection = args.collection
+    language = args.language
+    want_children = False if args.want_children == 'no' else True
+    syntax_type = args.syntax_type
+
+    syntax = get_syntax(syntax_type)
 
     getter.main(collection, language)
 
