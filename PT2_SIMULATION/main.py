d5dfafaf60920b212f3575fb64e2e091ca2d5c23
an-k45 <an_k27@protonmail.com>
2021-10-08 12:39:57 -0400

    Complete simulation setup

--- PT2_SIMULATION/main.py
+++ PT2_SIMULATION/main.py
@@ -1,4 +1,5 @@
 import numpy as np
+from tqdm import tqdm
 
 class Child(object):
     def __init__(self, lexicon, C, F):
@@ -8,9 +9,9 @@ class Child(object):
         self.classifier_count = np.zeros(self.C)  # no. seen occurences of each classifier
         self.classifier_features = np.zeros((self.C, self.F))  # no. feature observations with each classifier
 
-    def add_interaction(self, classifier_idx, noun):
-        self.classifier_count[classifier_idx] += 1
-        self.classifier_features[classifier_idx] += noun
+    def add_interaction(self, cl_idx, noun_idx):
+        self.classifier_count[cl_idx] += 1
+        self.classifier_features[cl_idx] += self.lexicon.get_noun(noun_idx)
 
 class Adult(object):
     def __init__(self, lexicon, C, F, initial_state):
@@ -19,10 +20,10 @@ class Adult(object):
         self.F = F  # no. features in lexicon
         self.classifier_state = initial_state
 
-    def get_pairable_classifiers_idxs(self, noun):
-        """ Given a noun vector (of size self.F) return the indices of pairable classifiers
+    def get_pairable_classifiers_idxs(self, noun_idx):
+        """ Given a noun index return the indices of pairable classifiers
         """
-        diff = np.tile(noun, (self.C, 1)) - self.classifier_state
+        diff = np.tile(self.lexicon.get_noun(noun_idx), (self.C, 1)) - self.classifier_state
         mins = np.min(diff, axis=1)
         min_idxs = np.argwhere(mins >= 0)
         return np.squeeze(min_idxs)
@@ -36,8 +37,14 @@ class Lexicon(object):
         self.lexicon_dist = self.init_lexicon_dist(lex_dist_type)
         self.nouns = self.init_nouns()
 
+    def normalize(self, v):
+        return v / np.sum(v)
+
     def init_lexicon_dist(self, lex_dist_type):
-        pass
+        if lex_dist_type == 'zipf':
+            return self.normalize(np.array([self.V/(i+1) for i in range(self.V)]))
+        elif lex_dist_type == 'uniform':
+            return self.normalize(np.ones(self.V))
 
     def init_nouns(self):
         # Limit a noun to up to 3 features for now. 
@@ -45,6 +52,14 @@ class Lexicon(object):
         for i in range(self.V): # TODO: numpy-ify this
             nouns[i][np.random.choice(self.F, 3)] = 1
         return nouns 
+
+    def get_random_noun_idxs(self, J):
+        """ Return J randomly chosen indices for nouns based on the lexical distribution
+        """
+        return np.random.choice(self.V, J, p=self.lexicon_dist)
+
+    def get_noun(self, i):
+        return self.nouns[i]
 
 
 class Simulation(object):
@@ -101,16 +116,17 @@ class Simulation(object):
         return arr
     
     def init_start_state(self):
-        for k in reversed(range(self.K)):
+        for k in tqdm(reversed(range(self.K))):
             for a in range(self.N - self.K):
                 targets = np.random.choice(self.K, self.I)
                 targets = targets[np.where(targets >= k)]
-                for j in range(self.J):
-                    # TODO: Select a noun
-                    # TODO: get viable classifiers via adult.get_pairable_classifiers_idxs(...)
-                    # TODO: select one randomly
-                    # TODO: run child.add_interaction(...)
-                    pass
+                for t in targets:
+                    # TODO: Select a noun via dist
+                    noun_idxs = self.lexicon.get_random_noun_idxs(self.J)
+                    for noun_idx in noun_idxs:
+                        cl_idx = np.random.choice(self.adults[a].get_pairable_classifiers_idxs(noun_idx))
+                        self.children[t].add_interaction(cl_idx, noun_idx)
+                        
 
     def simulate():
         # n individuals sorted by age (say, 100 or 1000?)
