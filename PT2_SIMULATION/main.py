7611d6ca1e50c3baaf4d554b977ee6922d6000ae
an-k45 <an_k27@protonmail.com>
2021-10-07 23:58:10 -0400

    Nearly complete simulation setup

--- PT2_SIMULATION/main.py
+++ PT2_SIMULATION/main.py
@@ -1,23 +1,54 @@
-class Child(object):
-    def __init__(self, lexicon):
-        self.lexicon = lexicon
-        self.classifier_count = {}  # no. seen occurences of each classifier
-        self.classifier_features = {}  # no. feature observations with each classifier
+import numpy as np
 
-# Child view of classifier for TP:
-# C1 = {"total": 10, "a": 7, "b": 3, "c": 1} --> n=10, then total count of appearence of features a,b,c with C1 via a noun
+class Child(object):
+    def __init__(self, lexicon, C, F):
+        self.lexicon = lexicon
+        self.C = C  # no. classifiers in lexicon
+        self.F = F  # no. features in lexicon
+        self.classifier_count = np.zeros(self.C)  # no. seen occurences of each classifier
+        self.classifier_features = np.zeros((self.C, self.F))  # no. feature observations with each classifier
+
+    def add_interaction(self, classifier_idx, noun):
+        self.classifier_count[classifier_idx] += 1
+        self.classifier_features[classifier_idx] += noun
 
 class Adult(object):
-    def __init__(self, lexicon):
+    def __init__(self, lexicon, C, F, initial_state):
         self.lexicon = lexicon
-        self.classifiers = {}
+        self.C = C  # no. classifiers in lexicon
+        self.F = F  # no. features in lexicon
+        self.classifier_state = initial_state
+
+    def get_pairable_classifiers_idxs(self, noun):
+        """ Given a noun vector (of size self.F) return the indices of pairable classifiers
+        """
+        diff = np.tile(noun, (self.C, 1)) - self.classifier_state
+        mins = np.min(diff, axis=1)
+        min_idxs = np.argwhere(mins >= 0)
+        return np.squeeze(min_idxs)
 
 class Lexicon(object):
-    def __init__(self, V, lexicon_dist):
-        super().__init__()
+    def __init__(self, V, C, F, lex_dist_type):
+        self.V = V  # no. nouns in lexicon
+        self.C = C  # no. classifiers in lexicon
+        self.F = F  # no. features in lexicon
+        
+        self.lexicon_dist = self.init_lexicon_dist(lex_dist_type)
+        self.nouns = self.init_nouns()
+
+    def init_lexicon_dist(self, lex_dist_type):
+        pass
+
+    def init_nouns(self):
+        # Limit a noun to up to 3 features for now. 
+        nouns = np.zeros((self.V, self.F))
+        for i in range(self.V): # TODO: numpy-ify this
+            nouns[i][np.random.choice(self.F, 3)] = 1
+        return nouns 
+
 
 class Simulation(object):
-    def __init__(self, N, K, V, I, J, productive, lexicon_dist):
+    def __init__(self, N, K, V, C, F, I, J, productive, lex_dist_type, classifier_init):
         """ Create feature sets, a set of nouns, and the initial generation.
 
         1. Setup
@@ -34,23 +65,52 @@ class Simulation(object):
         """
         self.N = N  # no. total individuals
         self.K = K  # no. children
+
         self.V = V  # no. nouns in lexicon
+        self.C = C  # no. classifiers in lexicon
+        self.F = F  # no. features in lexicon
+
         self.I = I  # no. interactions each adult partakes in
         self.J = J  # no. lexical items drawn per interaction
 
-        self.productive = productive  # method for productivity
-        self.lexicon_dist = lexicon_dist  # Dist. type of nouns in lexicon
-        assert self.productive in ["TP", "majority"]
+        self.productive = productive  # method for productivity: 'TP' or 'majority'
+        self.lex_dist_type = lex_dist_type  # Dist. type of nouns in lexicon: 'zipf' or 'uniform'
+        self.classifier_init = classifier_init  # Initial condition of classifiers: 'identity'
         
-        self.lexicon = Lexicon(self.V, self.lexicon_dist)
-        self.children = self.init_children(K, self.lexicon)
-        self.adults = self.init_adults(N - K, self.lexicon)
+        self.lexicon = Lexicon(self.V, self.C, self.F, self.lex_dist_type)
+        self.children = self.init_children()
+        self.adults = self.init_adults()
+        self.init_start_state()
     
-    def init_children(self, num, lexicon):
-        pass
+    def init_productive_classifiers(self):
+        if self.classifier_init == "identity":
+            assert self.C == self.F
+            return np.identity(self.C)
 
-    def init_adults(self, num, lexicon):
-        pass
+    def init_children(self):
+        arr = []
+        for _ in range(self.K):
+            arr.append(Child(self.lexicon, self.C, self.F))
+        return arr
+
+    def init_adults(self):
+        arr = []
+        inital_state = self.init_productive_classifiers()
+        for _ in range(self.N - self.K):
+            arr.append(Adult(self.lexicon, self.C, self.F, inital_state))
+        return arr
+    
+    def init_start_state(self):
+        for k in reversed(range(self.K)):
+            for a in range(self.N - self.K):
+                targets = np.random.choice(self.K, self.I)
+                targets = targets[np.where(targets >= k)]
+                for j in range(self.J):
+                    # TODO: Select a noun
+                    # TODO: get viable classifiers via adult.get_pairable_classifiers_idxs(...)
+                    # TODO: select one randomly
+                    # TODO: run child.add_interaction(...)
+                    pass
 
     def simulate():
         # n individuals sorted by age (say, 100 or 1000?)
