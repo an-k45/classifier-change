721a7cf635f8a4ed981384b8e54c4113b3950de0
Jordan Kodner <jkodner05@yahoo.com>
2022-02-13 11:40:25 -0500

    added full disambiguation stats

--- PT1_EMPIRICAL/tabulate_homophone_stats.py
+++ PT1_EMPIRICAL/tabulate_homophone_stats.py
@@ -17,11 +17,14 @@ def get_hstats(data):
     num_wordforms = 0
     num_hsets = 0
     num_disambig_hsets= 0
+    num_fulldisambig_hsets = 0
     num_majority_disambig_hsets = 0 # TODO
 
     num_nountoks = 0
     num_hphonetoks = 0
     num_disambig_hphonetoks = 0
+    num_fulldisambig_hphonetoks = 0
+    num_majority_disambig_hphonetoks = 0 # TODO
 
     for wordform, words in data.items():
         wf_numsylls = len(syll_rx.findall(wordform))
@@ -46,18 +49,26 @@ def get_hstats(data):
         if num_true > 1: # we know this wordform is homophonous 
             num_hsets += 1
             set_has_disambiguation = False
+            set_fulldisambiguated = True
             for i, cl_seti in enumerate(cl_sets):
                 is_disambiguated = False
+                fulldisambiguated = True
                 for j, cl_setj in enumerate(cl_sets):
                     if i == j: # don't compare against self
                         continue
                     if set(cl_seti.keys()).difference(set(cl_setj.keys())):
                         is_disambiguated = True
+                    if set(cl_seti.keys()).difference(set(cl_setj.keys())) != set(cl_seti.keys()):
+                        fulldisambiguated = False
                 if is_disambiguated:
                     num_disambig_hphones += 1
                     set_has_disambiguation = True
+                if not fulldisambiguated:
+                    set_fulldisambiguated = False
             if set_has_disambiguation:
                 num_disambig_hsets += 1
+            if set_fulldisambiguated:
+                num_fulldisambig_hsets += 1
 
         uniq_clsets = []
         for i, cl_seti in enumerate(cl_sets):
@@ -76,9 +87,10 @@ def get_hstats(data):
                     num_hphonetoks += count
                     if cl in uniqclset:
                         num_disambig_hphonetoks += count
-        
+                        if len(uniqclset) == len(clset):
+                            num_fulldisambig_hphonetoks += count
 
-    return {"W":num_wordforms, "N":num_nouns, "H":num_hphones, "h":num_disambig_hphones, "K":num_hsets, "k_1":num_disambig_hsets, "k_2":num_majority_disambig_hsets, "N_t":num_nountoks, "H_t":num_hphonetoks, "h_t":num_disambig_hphonetoks}
+    return {"W":num_wordforms, "N":num_nouns, "H":num_hphones, "h":num_disambig_hphones, "K":num_hsets, "k_1":num_disambig_hsets, "k_full":num_fulldisambig_hsets, "k_2":num_majority_disambig_hsets, "N_t":num_nountoks, "H_t":num_hphonetoks, "h_t":num_disambig_hphonetoks, "h_tfull":num_fulldisambig_hphonetoks}
 
 
 
@@ -89,17 +101,23 @@ def display_hstats(hstats, infname):
     print("W Number of wforms:\t\t\t\t\t%s" % hstats["W"])
     print("K Number of wforms w/ Homophones:\t\t\t%s" % hstats["K"])
     print("k_1 Number of wforms w/ Hphones Disambig'd by Cl:\t%s" % hstats["k_1"])
+    print("k_full Number of '' '' '' fully Disambig'd by Cl:\t%s" % hstats["k_full"])
     print("Percent wforms w Hphones K/W\t\t\t\t%s%%" % round(hstats["K"]/hstats["W"]*100,3))
     print("Percent Hphone wforms Disambig'd k_1/K\t\t\t%s%%" % round(hstats["k_1"]/hstats["K"]*100,3))
     print("Percent wforms Disambig'd k_1/W\t\t\t\t%s%%" % round(hstats["k_1"]/hstats["W"]*100,3))
+    print("Percent Hphone wforms '' k_full/K\t\t\t%s%%" % round(hstats["k_full"]/hstats["K"]*100,3))
+    print("Percent wforms '' k_full/W\t\t\t\t%s%%" % round(hstats["k_full"]/hstats["W"]*100,3))
     print()
     print("TOKENS:")
     print("N Number of Noun Toks:\t\t\t\t\t%s" % hstats["N_t"])
     print("H Number of Nouns Toks w/ Homophones:\t\t\t%s" % hstats["H_t"])
     print("h Number of Nouns Toks w/ Hphones Disambig'd by Cl:\t%s" % hstats["h_t"])
+    print("h_full Number of '' '' '' '' fully Disambig'd by Cl:\t%s" % hstats["h_tfull"])
     print("Percent Nouns Toks w/ Hphones H/N\t\t\t%s%%" % round(hstats["H_t"]/hstats["N_t"]*100,3))
     print("Percent Homophones Disambig'd h/H\t\t\t%s%%" % round(hstats["h_t"]/hstats["H_t"]*100,3))
     print("Percent Nouns Toks Disambig'd h/N\t\t\t%s%%" % round(hstats["h_t"]/hstats["N_t"]*100,3))
+    print("Percent '' fully Disambig'd h_full/H\t\t\t%s%%" % round(hstats["h_tfull"]/hstats["H_t"]*100,3))
+    print("Percent '' '' fully Disambig'd h_full/N\t\t\t%s%%" % round(hstats["h_tfull"]/hstats["N_t"]*100,3))
 
 
 
@@ -112,3 +130,8 @@ if __name__ == "__main__":
     data = read_data(args.infname)
     hstats = get_hstats(data)
     display_hstats(hstats, args.infname)
+
+#    dropnum = int(hstats["N_t"]/10)
+    for i in range(0,10):
+        hstats = get_hstats(data)
+        display_hstats(hstats, args.infname)
